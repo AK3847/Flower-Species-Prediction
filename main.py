6b6400bf35bc1a19c553bdbe37813efc6e00aea1
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-01-26 13:49:57 +0530

    added info on terminal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from model import predict
+from rich.console import Console
+console=Console()
 def start():
     predict()
 
 if __name__=='__main__':
+    console.print('A Python script to predict the Flower Species on the [italic]Iris DataSet[/italic] via [bold #638889]Decision Tree method.[/bold #638889]',style="#42855B")
     start()
