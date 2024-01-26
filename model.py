a3659ac87ffda94129068a05488b5dcc54edcf04
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-01-26 13:01:43 +0530

    made the basic model

--- model.py
+++ model.py
@@ -4,27 +4,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
-
+import rich
+from rich.console import Console
 from sklearn.datasets import load_iris
-iris = load_iris()
-X = iris.data
-y = iris.target
+console=Console()
+def make_model():
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-
-model = DecisionTreeClassifier(random_state=42)
-model.fit(X_train_scaled, y_train)
-
-y_pred = model.predict(X_test_scaled)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
 
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
+    model = DecisionTreeClassifier(random_state=1)
+    model.fit(X_train_scaled, y_train)
+    console.print(f'The Model has been succesfully made!',style="italic #42855B")
+    return model,X_test_scaled,y_test
 
-print(f"Accuracy: {accuracy}")
-print("Classification Report:\n", report)
+def accuracy(model,X_test_scaled,y_test):
+    y_pred = model.predict(X_test_scaled)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+
+    console.print(f"Accuracy: {accuracy}",style="#5272F2")
+    print("Classification Report:\n", report)
+
+
+def predict():
+    model,X_test_scaled,y_test=make_model()
+    s=input(f'Find accuracy (Y/n): ')
+    if(s=='y' or s=='Y'):
+        accuracy(model,X_test_scaled,y_test)
+    else:
+        print(f'Exiting....')
+        exit()
